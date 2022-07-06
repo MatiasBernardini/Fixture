f4a68510f7df8cf3036ac10b0ca9526c67c8a12d
matiasbernardini <bernardinimatias14@gmail.com>
2022-07-06 18:00:14 -0300

    modificando nodos

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ function eliminadoOctavos () {
     document.getElementById('eliminadoEnOctavos').style.display = 'flex';
 }
 
+let mensajeEliminadoOctavos = document.getElementById ("textoEliminadoOctavos")
+mensajeEliminadoOctavos.className = "mt-4"
+
 function mostrarCuartos () {
     document.getElementById('cuartos').style.display = 'flex';
 }
+
+function eliminadoCuartos () {
+    document.getElementById('eliminadoEnCuartos').style.display = 'flex';
+}
+
+let mensajeEliminadoCuartos = document.getElementById ("textoEliminadoCuartos")
+mensajeEliminadoCuartos.className = "mt-4"
 
 function mostrarSemis () {
     document.getElementById('semis').style.display = 'flex';
