817b53ea302032043f7f9375b9dfbfcb5914f0f4
matiasbernardini <bernardinimatias14@gmail.com>
2022-07-05 23:20:50 -0300

    html ya creado

---
+++ index.js
@@ -0,0 +1,19 @@
+function mostrarOctavos () {
+    document.getElementById('octavos').style.display = 'flex';
+}
+
+function eliminadoOctavos () {
+    document.getElementById('eliminadoEnOctavos').style.display = 'flex';
+}
+
+function mostrarCuartos () {
+    document.getElementById('cuartos').style.display = 'flex';
+}
+
+function mostrarSemis () {
+    document.getElementById('semis').style.display = 'flex';
+}
+
+function mostrarFinal () {
+    document.getElementById('final').style.display = 'flex';
+}
